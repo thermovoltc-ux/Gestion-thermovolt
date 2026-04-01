5e67a68c183a4aed16c38da2e1a557cd62e1a127
Juan Esteban <juan@example.com>
2026-03-31 20:37:13 -0500

    Add back allauth AccountMiddleware as required

--- gestion_mantenimiento/gestion_mantenimiento/settings.py
+++ gestion_mantenimiento/gestion_mantenimiento/settings.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'gestion_mantenimiento.gestion_mantenimiento.urls'
