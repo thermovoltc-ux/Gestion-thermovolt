7fbe8a261e859ee8646c75b4cebc7eca1f0b349c
Juan Esteban <juan@example.com>
2026-04-19 15:01:18 -0500

    Redirect accounts login to custom login page with styles

--- gestion_mantenimiento/urls.py
+++ gestion_mantenimiento/urls.py
@@ -9,10 +9,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('solicitudes/', include('gestion_mantenimiento.solicitudes.urls')),
     path('users/', include('gestion_mantenimiento.users.urls')),
+    path('accounts/login/', lambda request: redirect('custom_login', permanent=False)),
     path('accounts/', include('allauth.urls')),  # Asegúrate de incluir esta línea
     path('Activos/', include('gestion_mantenimiento.Activos.urls')),
     path('Gestion_ot/', include('gestion_mantenimiento.Gestion_ot.urls')),
-    path('', lambda request: redirect('accounts/login/', permanent=False)),  # Redirige la raíz al login
+    path('', lambda request: redirect('custom_login', permanent=False)),  # Redirige la raíz al login personalizado
 ]
 
 if settings.DEBUG:
