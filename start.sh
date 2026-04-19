6bbbc5a84e1565595a6a2b310f452b777ec485cb
Juan Esteban <juan@example.com>
2026-04-19 16:01:59 -0500

    Clear staticfiles before collectstatic in production startup

--- start.sh
+++ start.sh
@@ -59,7 +59,7 @@ echo "STARTUP: database path"
 DJANGO_SETTINGS_MODULE=gestion_mantenimiento.settings python -c "import django, os; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_mantenimiento.settings'); django.setup(); from django.conf import settings; print(settings.DATABASES['default'].get('NAME'))"
 
 echo "STARTUP: collecting static files"
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 echo "STARTUP: launching gunicorn"
 exec gunicorn gestion_mantenimiento.wsgi --bind 0.0.0.0:$PORT --workers 1 --threads 2
