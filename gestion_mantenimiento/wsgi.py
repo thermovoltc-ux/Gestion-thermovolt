a153c4b0e2b336bef09958ab41eb36763a4cb960
Juan Esteban <juan@example.com>
2026-04-18 21:29:28 -0500

    Fix DJANGO_SETTINGS_MODULE in wsgi.py

--- gestion_mantenimiento/wsgi.py
+++ gestion_mantenimiento/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_mantenimiento.gestion_mantenimiento.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_mantenimiento.settings')
 
 application = get_wsgi_application()
