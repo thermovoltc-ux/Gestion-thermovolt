c65bfe19b12a85a29199c8389b99abbba84432ab
Juan Esteban <juan@example.com>
2026-09-25 12:28:52 -0500

    refactor: eliminar gestion_mantenimiento/static/ duplicado (fuente unica: static/ raiz)

--- gestion_mantenimiento/static/js/gestion_ot.js
+++
@@ -1,7 +0,0 @@
-import './csrf.js';
-import './kanbanColumns.js';
-import './calendarView.js';
-import './modals.js';
-
-// Este módulo agrupa la lógica de gestión OT / kanban para mantener separación con solicitudes.
-
